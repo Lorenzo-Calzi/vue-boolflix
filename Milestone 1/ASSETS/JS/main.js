06fdbf2ded20988377eb9224ee4c812b9034d4be
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-08 12:08:36 +0200

    Aggiunta ricerca dinamica

--- Milestone 1/ASSETS/JS/main.js
+++ Milestone 1/ASSETS/JS/main.js
@@ -2,21 +2,24 @@ const app = new Vue({
     el: '#app',
 
     data: {
-        url: 'https://api.themoviedb.org/3/search/movie?api_key=e9adcb3a8787e160dfea6702af87220c&query=Back to the Future Part III',
+        url: 'https://api.themoviedb.org/3/search/movie?api_key=e9adcb3a8787e160dfea6702af87220c&query=',
         filmData: [],
-
+        search: '',
     }, 
 
     methods: {
-
+        searchFilm() {
+            console.log(this.search);
+        }
     },
 
     mounted() {
-        const fullUrl = this.url;
+        const fullUrl = this.url + this.search;
         axios
             .get(fullUrl)
             .then(resp => {
-                this.filmData = resp.data.response;
+                this.filmData = resp.data.results;
+                console.log(this.filmData);
             })
 
             .catch(e => {

afc40f02d6638c91258fbcdf12690bff5f026b92
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-08 14:50:18 +0200

    Correzione funzione per ricerca film

--- Milestone 1/ASSETS/JS/main.js
+++ Milestone 1/ASSETS/JS/main.js
@@ -5,18 +5,21 @@ const app = new Vue({
         url: 'https://api.themoviedb.org/3/search/movie?api_key=e9adcb3a8787e160dfea6702af87220c&query=',
         filmData: [],
         search: '',
+        fullUrl: ''
     }, 
 
     methods: {
         searchFilm() {
-            console.log(this.search);
+            fullUrl = this.url + this.search
+            console.log(fullUrl);
+            return fullUrl
         }
     },
 
     mounted() {
-        const fullUrl = this.url + this.search;
+        /* const fullUrl = this.url ; */
         axios
-            .get(fullUrl)
+            .get(this.fullUrl)
             .then(resp => {
                 this.filmData = resp.data.results;
                 console.log(this.filmData);
@@ -28,3 +31,6 @@ const app = new Vue({
     }
 
 })
+
+
+

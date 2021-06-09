0d6a9c535605118d889021bd6cb8ef7a778bd612
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-09 14:37:06 +0200

    Aggiunto hover sulle card e ridefinito lo style

---
+++ Milestone 4/ASSETS/JS/main.js
@@ -0,0 +1,61 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        fullUrl: '',
+        search: '',
+        error: null,
+        flags: ['it', 'en'], // if includes 
+
+        urlFilm: 'https://api.themoviedb.org/3/search/movie?api_key=e9adcb3a8787e160dfea6702af87220c&query=',
+        filmData: [],
+        
+        urlSerie: 'https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=',
+        serieData: [],
+    }, 
+
+    methods: {  
+        searchFilm() {
+            fullUrl = this.urlFilm + this.search;
+
+            axios
+            .get(fullUrl)
+            .then(resp => {
+                this.filmData = resp.data;
+            })
+
+            .catch(e => {
+                console.error(e);
+                this.error = 'Sorry something went Wrong! ' + e;
+            })
+
+        },
+
+        searchSerie(){
+            fullUrl = this.urlSerie + this.search;
+
+            axios
+            .get(fullUrl)
+            .then(resp => {
+                this.serieData = resp.data;
+            })
+
+            .catch(e => {
+                console.error(e)
+                this.error = 'Sorry something went Wrong! ' + e;
+            })
+        },
+        
+    },
+
+    mounted() {
+
+    }
+})
+
+
+
+
+
+
+
